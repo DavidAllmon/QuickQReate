11e6175f26a88099f3ed76239a9edf3d9e043681
DavidAllmon <admin@davidallmon.dev>
2024-08-28 14:34:34 -0400

    Updated Background

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gradient-to-br from-gray-900 via-purple-900 to-violet-900`}>
+        {children}
+      </body>
     </html>
   );
 }
